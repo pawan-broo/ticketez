bb972c6b71dedbc415651a6a3320a749d2b45967
Prashant Singh <prashant.s2922@gmail.com>
2025-10-27 18:42:44 +0530

    db setup

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,18 +1,24 @@
 import { nextCookies } from 'better-auth/next-js';
-import { betterAuth, type BetterAuthOptions } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@ticketez/db";
-import * as schema from "@ticketez/db/schema/auth";
+import { betterAuth, type BetterAuthOptions } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { db } from '@ticketez/db';
+import * as schema from '@ticketez/db/schema/auth';
+import { randomUUID } from 'crypto';
 
 export const auth = betterAuth<BetterAuthOptions>({
-	database: drizzleAdapter(db, {
-		provider: "pg",
+  database: drizzleAdapter(db, {
+    provider: 'pg',
 
-		schema: schema,
-	}),
-	trustedOrigins: [process.env.CORS_ORIGIN || ""],
-	emailAndPassword: {
-		enabled: true,
-	},
-  plugins: [nextCookies()]
+    schema: schema,
+  }),
+  trustedOrigins: [process.env.CORS_ORIGIN || ''],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
+  advanced: {
+    database: {
+      generateId: () => randomUUID(),
+    },
+  },
 });
