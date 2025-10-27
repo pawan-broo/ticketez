edc8f2226c7efe1eca3653a778f960c53815960d
Prashant Singh <prashant.s2922@gmail.com>
2025-10-27 18:26:20 +0530

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,18 @@
+import { nextCookies } from 'better-auth/next-js';
+import { betterAuth, type BetterAuthOptions } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@ticketez/db";
+import * as schema from "@ticketez/db/schema/auth";
+
+export const auth = betterAuth<BetterAuthOptions>({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+
+		schema: schema,
+	}),
+	trustedOrigins: [process.env.CORS_ORIGIN || ""],
+	emailAndPassword: {
+		enabled: true,
+	},
+  plugins: [nextCookies()]
+});
